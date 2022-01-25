d8663ff524ee040051a52cb70dce83ca2d357af9
massoudi malek <malek.massoudi@groupeavril.com>
2022-01-25 20:51:25 +0100

    project clean up WIP

--- web/modules/custom/fizz_buzz/src/Plugin/rest/resource/FizzBuzzResource.php
+++ web/modules/custom/fizz_buzz/src/Plugin/rest/resource/FizzBuzzResource.php
@@ -35,7 +35,8 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
  * For accessing Drupal entities through REST interface use
  * \Drupal\rest\Plugin\rest\resource\EntityResource plugin.
  */
-class FizzBuzzResource extends ResourceBase implements DependentPluginInterface {
+class FizzBuzzResource extends ResourceBase implements DependentPluginInterface
+{
 
   /**
    * The database connection.
@@ -60,7 +61,8 @@ class FizzBuzzResource extends ResourceBase implements DependentPluginInterface 
    * @param \Drupal\Core\Database\Connection $db_connection
    *   The database connection.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, array $serializer_formats, LoggerInterface $logger, Connection $db_connection) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, array $serializer_formats, LoggerInterface $logger, Connection $db_connection)
+  {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
     $this->dbConnection = $db_connection;
   }
@@ -68,7 +70,8 @@ class FizzBuzzResource extends ResourceBase implements DependentPluginInterface 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+  {
     return new static(
       $configuration,
       $plugin_id,
@@ -90,81 +93,19 @@ class FizzBuzzResource extends ResourceBase implements DependentPluginInterface 
    *
    * @throws \Symfony\Component\HttpKernel\Exception\HttpException
    */
-  public function get($id) {
-    return new ResourceResponse($this->loadRecord($id));
+  public function get($id)
+  {
+    $this->logger->notice('FizzBuzz record @id has been requested.', ['@id' => $id]);
+    return new ResourceResponse(["test"]);
   }
 
-  /**
-   * Responds to POST requests and saves the new record.
-   *
-   * @param mixed $data
-   *   Data to write into the database.
-   *
-   * @return \Drupal\rest\ModifiedResourceResponse
-   *   The HTTP response object.
-   */
-  public function post($data) {
 
-    $this->validate($data);
-
-    $id = $this->dbConnection->insert('fizz_buzz')
-      ->fields($data)
-      ->execute();
-
-    $this->logger->notice('New fizzbuzz record has been created.');
-
-    $created_record = $this->loadRecord($id);
-
-    // Return the newly created record in the response body.
-    return new ModifiedResourceResponse($created_record, 201);
-  }
-
-  /**
-   * Responds to entity PATCH requests.
-   *
-   * @param int $id
-   *   The ID of the record.
-   * @param mixed $data
-   *   Data to write into the database.
-   *
-   * @return \Drupal\rest\ModifiedResourceResponse
-   *   The HTTP response object.
-   */
-  public function patch($id, $data) {
-    $this->validate($data);
-    return $this->updateRecord($id, $data);
-  }
-
-  /**
-   * Responds to entity DELETE requests.
-   *
-   * @param int $id
-   *   The ID of the record.
-   *
-   * @return \Drupal\rest\ModifiedResourceResponse
-   *   The HTTP response object.
-   *
-   * @throws \Symfony\Component\HttpKernel\Exception\HttpException
-   */
-  public function delete($id) {
-
-    // Make sure the record still exists.
-    $this->loadRecord($id);
-
-    $this->dbConnection->delete('fizz_buzz')
-      ->condition('id', $id)
-      ->execute();
-
-    $this->logger->notice('FizzBuzz record @id has been deleted.', ['@id' => $id]);
-
-    // Deleted responses have an empty body.
-    return new ModifiedResourceResponse(NULL, 204);
-  }
 
   /**
    * {@inheritdoc}
    */
-  protected function getBaseRoute($canonical_path, $method) {
+  protected function getBaseRoute($canonical_path, $method)
+  {
     $route = parent::getBaseRoute($canonical_path, $method);
 
     // Change ID validation pattern.
@@ -178,14 +119,16 @@ class FizzBuzzResource extends ResourceBase implements DependentPluginInterface 
   /**
    * {@inheritdoc}
    */
-  public function calculateDependencies() {
+  public function calculateDependencies()
+  {
     return [];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function routes() {
+  public function routes()
+  {
     $collection = parent::routes();
 
     // Take out BC routes added in base class.
@@ -199,86 +142,4 @@ class FizzBuzzResource extends ResourceBase implements DependentPluginInterface 
 
     return $collection;
   }
-
-  /**
-   * Validates incoming record.
-   *
-   * @param mixed $record
-   *   Data to validate.
-   *
-   * @throws \Symfony\Component\HttpKernel\Exception\BadRequestHttpException
-   */
-  protected function validate($record) {
-    if (!is_array($record) || count($record) == 0) {
-      throw new BadRequestHttpException('No record content received.');
-    }
-
-    $allowed_fields = [
-      'title',
-      'description',
-      'price',
-    ];
-
-    if (count(array_diff(array_keys($record), $allowed_fields)) > 0) {
-      throw new BadRequestHttpException('Record structure is not correct.');
-    }
-
-    if (empty($record['title'])) {
-      throw new BadRequestHttpException('Title is required.');
-    }
-    elseif (isset($record['title']) && strlen($record['title']) > 255) {
-      throw new BadRequestHttpException('Title is too big.');
-    }
-    // @DCG Add more validation rules here.
-  }
-
-  /**
-   * Loads record from database.
-   *
-   * @param int $id
-   *   The ID of the record.
-   *
-   * @return array
-   *   The database record.
-   *
-   * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
-   */
-  protected function loadRecord($id) {
-    $record = $this->dbConnection->query('SELECT * FROM {fizz_buzz} WHERE id = :id', [':id' => $id])->fetchAssoc();
-    if (!$record) {
-      throw new NotFoundHttpException('The record was not found.');
-    }
-    return $record;
-  }
-
-  /**
-   * Updates record.
-   *
-   * @param int $id
-   *   The ID of the record.
-   * @param array $record
-   *   The record to validate.
-   *
-   * @return \Drupal\rest\ModifiedResourceResponse
-   *   The HTTP response object.
-   */
-  protected function updateRecord($id, array $record) {
-
-    // Make sure the record already exists.
-    $this->loadRecord($id);
-
-    $this->validate($record);
-
-    $this->dbConnection->update('fizz_buzz')
-      ->fields($record)
-      ->condition('id', $id)
-      ->execute();
-
-    $this->logger->notice('FizzBuzz record @id has been updated.', ['@id' => $id]);
-
-    // Return the updated record in the response body.
-    $updated_record = $this->loadRecord($id);
-    return new ModifiedResourceResponse($updated_record, 200);
-  }
-
 }
